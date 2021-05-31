0ea79208d25ac37ce63b96530c656a6beb2c8915
matty-bennett <bennyot10@gmail.com>
2021-05-31 17:13:32 -0600

    updated User model

--- models/User.js
+++ models/User.js
@@ -3,17 +3,30 @@ const { Schema, model } = require('mongoose');
 const UserSchema = new Schema(
     {
         username: {
-
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
         },
         email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/([a-zA-Z0-9_\-\.]+)@([a-zA-Z]+).([a-z]{2,6})/, 'Please enter a valid e-mail address']
 
         },
-        thoughts: [],
-        friends: []
+        thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought'}],
+        friends: [{ type: Schema.Types.ObjectId, ref: 'User'}]
     },
     {
         toJSON: {
-            virtuals: true
-        }
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-)
+);
+
+const User = model('User', UserSchema);
+
+module.exports = User;
